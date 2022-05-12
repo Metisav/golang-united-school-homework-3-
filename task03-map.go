5c01f0a9bd3f226ceb903877a240d4653170b128
av.vetkin <av.vetkin@tensor.ru>
2022-05-12 16:12:20 +0500

    first try

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,20 @@
 package homework
 
+import (
+	"sort"
+)
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
+	keys := make([]int, len(input))
+	result = make([]string, len(input))
+	i := 0
+	for mapKey := range input {
+		keys[i] = mapKey
+		i++
+	}
+	sort.Ints(keys)
+	for i := range keys {
+		result[i] = input[keys[i]]
+	}
 	return
 }
